d0e84b63ebdc907c2dbcf21d77d1362f760370b0
charleseMuchmore <87589934+charleseMuchmore@users.noreply.github.com>
2021-10-30 19:18:49 -0700

    tidied up some stuff

--- A_Main.py
+++ A_Main.py
@@ -1,28 +1,21 @@
 #imports#
 from B_Functions import *
-
 #greeting#
 greet_user()
 rangee = rangee()
 # print(rangee)
-
 #difficulty#
 difficulty = choose_difficulty()
-
 #starting num of attempts based on difficulty level#
 attempts_left = determine_num_attempts(difficulty)
 display_num_attempts_left(attempts_left)
-
 #info#
 the_number = randint(1, rangee)
-# print(f"the number is {the_number}")
-
 #condition block#
 loop_parameter = 'not yet'
 while loop_parameter == 'not yet':
     #guess#
     guess = input("Make a guess: ")
-
     loop_parameter = logic_guess(guess, the_number, rangee)
     attempts_left -= 1
     if loop_parameter == 'not yet':
--- B_Functions.py
+++ B_Functions.py
@@ -1,5 +1,4 @@
 from random import randint
-
 def determine_num_attempts(difficulty_level):
     """Number of attempts logic, returns the number of attempts available"""
     if difficulty_level == 'easy':
@@ -8,30 +7,21 @@ def determine_num_attempts(difficulty_level):
         return 5
     else:
         print(f"I'm sorry, but '{difficulty_level}' is not a valid input.")
-
 def greet_user():
     """Simply prints a greeting for the game"""
     print("Welcome to the Number Guessing Game!")
-
 def rangee():
     """has a local range variable, tells user the range, returns the max num of the range"""
     rangee = 100
     print(f"I'm thinking of a number between 1 and {rangee}.")
     return rangee
-
 def choose_difficulty():
     """Returns a user input of difficulty level"""
     return input("Choose a difficulty. Type 'easy' or 'hard': ")
-
 def display_num_attempts_left(current_num_attempts):
     """displays and returns current number of attempts"""
     print(f"You have {current_num_attempts} attempts remaining to guess the number")
     return current_num_attempts
-
-# def guess():
-#     """asks for and returns the user's guess"""
-#     return input("Make a guess: ")
-
 def logic_guess(guess, number, rangee):
     """Take the user's guess, the generated number, and the range and prints messages to the user to help them figure it out.
      Returns 'win' or 'not yet'"""
